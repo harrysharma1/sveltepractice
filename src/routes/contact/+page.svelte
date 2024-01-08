a074ad6e51eb1f8fad1472b26b9e58b9ca8cfc2d
Harry Sharma <harrysharma1066@gmail.com>
2024-01-08 18:40:22 +0000

    update: move navbar to template file

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,23 +1,5 @@
-<nav class="flex items-center justify-between flex-wrap bg-teal-500 p-6">
-    <div class="flex items-center flex-shrink-0 text-white mr-6">
-      <span class="font-semibold text-xl tracking-tight"> <a href="/">harrysharma1</a> </span>
-    </div>
-    <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-      <div class="text-sm lg:flex-grow">
-        <a href="/" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
-          Home
-        </a>
-        <a href="/about" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
-          About 
-        </a>
-        <a href="/contact" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
-          Contact
-        </a>
-      </div>
-      <div>
-        <a href="" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Download</a>
-      </div>
-    </div>
-  </nav>
-
+<script>
+    import Navbar from "../../template/Navbar.svelte";
+</script>
+<Navbar/>
 <h1 class="text-5xl">Contact</h1>
