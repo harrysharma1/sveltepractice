8d8a51bd89995fd9ca8bd05cc763569aca7c0eee
Harry Sharma <harrysharma1066@gmail.com>
2024-01-09 15:42:58 +0000

    add: form

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -2,4 +2,10 @@
     import Navbar from "../../template/Navbar.svelte";
 </script>
 <Navbar/>
-<h1 class="text-5xl">Contact</h1>
+<div class="justify-center">
+    <h1 class="text-5xl">Contact</h1>
+    <form>
+        <input type="email" name="email" class="bg-slate-300 outline-dotted">
+    </form>
+</div>
+
