425fb11a27b4c1cc0a9d59c8bfa6594e2d7264ec
Harry Sharma <harrysharma1066@gmail.com>
2024-01-10 11:01:57 +0000

    update: form style

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,11 +1,23 @@
 <script>
     import Navbar from "../../template/Navbar.svelte";
+    let encodedString 
 </script>
 <Navbar/>
-<div class="justify-center">
-    <h1 class="text-5xl">Contact</h1>
-    <form>
-        <input type="email" name="email" class="bg-slate-300 outline-dotted">
-    </form>
-</div>
+<div class="h-screen flex flex-col  
+items-center justify-center"> 
+<p class="text-teal-500 text-xl mb-3"> 
+Contact
+</p> 
+
+<form> 
+
+<textarea aria-label="Enter subject" placeholder="Enter subject ..." class="text-sm text-gray-base w-full mr-3  py-5 px-4 h-[75px] border border-gray-200  rounded mb-2" style="resize: none;"></textarea>
+<textarea aria-label="Enter body" placeholder="Enter body ..." class="text-sm text-gray-base w-full mr-3  py-5 px-4 h-[250px] border border-gray-200  rounded mb-2" style="resize: none;"></textarea>
+
+<button type="submit"
+    class="bg-teal-500 hover:bg-teal-700 w-full mt-4 rounded mb-2 text-white"> 
+Login 
+</button> 
+</form> 
+</div> 
 
