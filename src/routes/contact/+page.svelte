9b58afa29e8b20ba71b80598eb075d6e49ff8b95
Harry Sharma <harrysharma1066@gmail.com>
2024-01-10 11:10:23 +0000

    add: email encoded string

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,6 +1,12 @@
 <script>
     import Navbar from "../../template/Navbar.svelte";
-    let encodedString 
+    let encodedString = "mailto:harrysharma1066@gmail.com?subject=test_subject&body=test_body";
+    let emailSubject = "";
+    let emailBody = "";
+    
+    function updateEncodedString() {
+        encodedString = `mailto:harrysharma1066@gmail.com?subject=${encodeURIComponent(emailSubject)}&body=${encodeURIComponent(emailBody)}`;
+    }
 </script>
 <Navbar/>
 <div class="h-screen flex flex-col  
@@ -9,15 +15,15 @@ items-center justify-center">
 Contact
 </p> 
 
-<form> 
+<form on:submit|preventDefault={updateEncodedString}> 
 
-<textarea aria-label="Enter subject" placeholder="Enter subject ..." class="text-sm text-gray-base w-full mr-3  py-5 px-4 h-[75px] border border-gray-200  rounded mb-2" style="resize: none;"></textarea>
-<textarea aria-label="Enter body" placeholder="Enter body ..." class="text-sm text-gray-base w-full mr-3  py-5 px-4 h-[250px] border border-gray-200  rounded mb-2" style="resize: none;"></textarea>
+<textarea bind:value={emailSubject} id="email_subject" aria-label="Enter subject" placeholder="Enter subject ..." class="text-sm text-gray-base w-full mr-3  py-5 px-4 h-[75px] border border-gray-200  rounded mb-2" style="resize: none;"></textarea>
+<textarea bind:value={emailBody} id="email_body" aria-label="Enter body" placeholder="Enter body ..." class="text-sm text-gray-base w-full mr-3  py-5 px-4 h-[250px] border border-gray-200  rounded mb-2" style="resize: none;"></textarea>
 
 <button type="submit"
     class="bg-teal-500 hover:bg-teal-700 w-full mt-4 rounded mb-2 text-white"> 
-Login 
+Send 
 </button> 
 </form> 
-</div> 
+</div>
 
