4ffb3a7022c349e8e5ff6e66e9735b0fe0079708
Harry Sharma <harrysharma1066@gmail.com>
2024-01-11 15:50:53 +0000

    add: data to timeline

--- src/routes/experience/+page.svelte
+++ src/routes/experience/+page.svelte
@@ -1,25 +1,61 @@
 <script>
     import Navbars from "../../template/Navbars.svelte";
-    import { Center, Text, Timeline } from '@svelteuidev/core';
+    import { Center, Text, Timeline, Stack } from '@svelteuidev/core';
 </script>
 <Navbars/>
 <div class="flex justify-center content-center p-32">
     <Center>
         <Timeline active={3} color="teal" bulletSize={15} lineWidth={2}>
             <Timeline.Item title="GCSE">
-                    <Text color="dimmed" size="sm">
-                            2014-2019 The Duston School
-                    </Text>
+                    <Stack align="strech" spacing="xs">
+                                <Text color="dimmed" size="sm">
+                                        2014-2019 The Duston School
+                                </Text>
+                                <Text color="dimmed" size="sm">• Maths: 8</Text>
+                                <Text color="dimmed" size="sm">• Geography: 9</Text>
+                                <Text color="dimmed" size="sm">• Combined Science: 8/7</Text>
+                                <Text color="dimmed" size="sm">• History: 8</Text>
+                                <Text color="dimmed" size="sm">• English Literature: 7</Text>
+                                <Text color="dimmed" size="sm">• English Language: 6</Text>
+                                <Text color="dimmed" size="sm">• Computer Science: 6</Text>
+                                <Text color="dimmed" size="sm">• Business Studies LM2</Text>
+                    </Stack>
             </Timeline.Item>
             <Timeline.Item title="A-Levels">
-                    <Text color="dimmed" size="sm">
+                    <Stack align="strech" spacing="xs">
+                        <Text color="dimmed" size="sm">
                             2019-2021 The Duston School
-                    </Text>
+                        </Text>
+                        <Text color="dimmed" size="sm">• Maths: A</Text>
+                        <Text color="dimmed" size="sm">• Computer Science: B</Text>
+                        <Text color="dimmed" size="sm">• Chemistry: B</Text>
+
+                    </Stack>
             </Timeline.Item>
             <Timeline.Item title="BSc Computer Science">
-                    <Text color="dimmed" size="sm">
-                        2021-2023 King's College London
-                    </Text>
+                    <Stack align="strech" spacing="xs">
+                        <Text color="dimmed" size="sm">
+                            2021-2023 King's College London
+                        </Text>
+                        <Text color="dimmed" size="sm">First Year:</Text>
+                        <Text color="dimmed" size="sm">• --Too be filled-- </Text>
+                        <Text color="dimmed" size="sm">• --Too be filled-- </Text>
+                        <Text color="dimmed" size="sm">• --Too be filled-- </Text>
+                        <Text color="dimmed" size="sm">• --Too be filled-- </Text>
+                        <Text color="dimmed" size="sm">• --Too be filled-- </Text>
+                        <Text color="dimmed" size="sm">• --Too be filled-- </Text>
+                        <Text color="dimmed" size="sm">• --Too be filled-- </Text>
+                        <Text color="dimmmed" size="sm">Second Year:</Text>
+                        <Text color="dimmed" size="sm">• --Too be filled-- </Text>
+                        <Text color="dimmed" size="sm">• --Too be filled-- </Text>
+                        <Text color="dimmed" size="sm">• --Too be filled-- </Text>
+                        <Text color="dimmed" size="sm">• --Too be filled-- </Text>
+                        <Text color="dimmed" size="sm">• --Too be filled-- </Text>
+                        <Text color="dimmed" size="sm">• --Too be filled-- </Text>
+                        <Text color="dimmed" size="sm">• --Too be filled-- </Text>
+
+
+                    </Stack>
             </Timeline.Item>
             <Timeline.Item title="Research Operations Engineer Intern">
                     <Text color="dimmed" size="sm">
